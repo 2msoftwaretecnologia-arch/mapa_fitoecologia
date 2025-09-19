abb39954231fdaad2d192540d0472f08cfdbaa20
marco antonio <2msoftware@gmail.com>
2025-09-19 17:22:56 -0300

    adaptando o codigo pra ele receber outros tipos de legenda

--- functions/manipular_arcgis/ajustar_quadrados.py
+++ functions/manipular_arcgis/ajustar_quadrados.py
@@ -24,21 +24,7 @@ from functions.outras_funcoes.helpers import *
 from text.tamanhos_e_descricoes import *
 
 def ajustar_quadrados_mapa():
-    if Text_infos.tipo_mapa == 'Fitoecologia':
-        itens_mapa_atual = criar_interface_opcoes(opcoes_disponiveis=['Floresta Estacional', 'Floresta Ombrófila Aberta', 'Floresta Ombrófila Densa', 'Savana Gramíneo Lenhosa', 'Savana Arborizada/Arbórea', 'Savana Florestada', 'Savana Parque', 'Rio'])
-        Text_infos.fito_ecologias = itens_mapa_atual
-        estilo_atual = estilos_fitoecologias
-        
-        Text_infos.descricao_mapa_atual = tamanhos_regioes_fito_ecologias
-    
-
-    if Text_infos.tipo_mapa == 'Geologia':
-        itens_mapa_atual = criar_interface_opcoes(opcoes_disponiveis=["Cráton Amazônico","Faixa Brasília","Grupo Bambuí","Bacia do Parnaíba","Coberturas Cenozóicas","Província Aurífera","Províncias de Níquel e Cromo","Depósitos de Fosfato e Calcário"])
-        Text_infos.geologias = itens_mapa_atual
-        estilo_atual = estilos_regioes_geologicas
-        
-        Text_infos.descricao_mapa_atual = tamanhos_geologicos
-    Text_infos.quantidade_necessario_mapa_atual = len(itens_mapa_atual)
+    estilo_atual = estilos_atuais()
     x_tamanho,y_tamanho = tamano_quadrados(Text_infos.quantidade_necessario_mapa_atual)
     lista_posicao = posicao_quadrados(Text_infos.quantidade_necessario_mapa_atual)
     click(coordinates.x_espaco_Branco,coordinates.y_espaco_Branco)
@@ -50,7 +36,7 @@ def ajustar_quadrados_mapa():
     x_desenhar_quadradro, y_desenhar_quadradro = capturar_clique("Clique em um lugar para desenhar o quadrado")
     coordinates.x_desenhar_quadradro = x_desenhar_quadradro
     coordinates.y_desenhar_quadradro = y_desenhar_quadradro
-    for posicoes,tipo in zip(lista_posicao,itens_mapa_atual):
+    for posicoes,tipo in zip(lista_posicao,Text_infos.itens_atuais):
         click(coordinates.x_retangulo,coordinates.y_retangulo,tempo=0.2)
         pressionar_tecla('r')
         desenhar_quadrado(coordinates.x_desenhar_quadradro,coordinates.y_desenhar_quadradro,largura=80)
